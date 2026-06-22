f55ffd6344ce8da9ffa1f38f9ca58c0c0fe7f3e8
Asher Feinstein <asher.feinstein@gmail.com>
2026-06-22 12:37:00 -0400

    Added basic thruster art

--- ship.gd
+++ ship.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 @onready var Ship_Part = preload("res://ship_part.tscn")
+@onready var Thruster = preload("res://thruster.tscn")
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("test"):
-		print("test")
 		var New_part = Ship_Part.instantiate()
 		add_child(New_part)
+	if Input.is_action_just_pressed("test2"):
+		var New_thruster = Thruster.instantiate()
+		add_child(New_thruster)
