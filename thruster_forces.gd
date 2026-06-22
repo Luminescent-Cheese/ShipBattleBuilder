28bfe853c651abf7193f0a8af888d35c07d59d47
Asher Feinstein <asher.feinstein@gmail.com>
2026-06-22 14:16:03 -0400

    thruster particle effects

--- thruster_forces.gd
+++ thruster_forces.gd
@@ -4,7 +4,9 @@ signal thrust
 
 @onready var ThrustDirection = $"../ThrustDirection"
 @onready var Thruster = $".."
+@onready var ThrustParticles = $"../ThrusterParticles"
 func _process(delta: float) -> void:
+	ThrustParticles.emitting = true
 	if Input.is_action_pressed("Thrust Forward") and snappedf(Thruster.rotation,0.01) == 0:
 		thrust.emit(ThrustDirection.global_position)
 	elif Input.is_action_pressed("Thrust Left") and snappedf(Thruster.rotation,0.01) == snappedf((3*PI)/2,0.01):
@@ -13,3 +15,5 @@ func _process(delta: float) -> void:
 		thrust.emit(ThrustDirection.global_position)
 	elif Input.is_action_pressed("Thrust Backward") and snappedf(Thruster.rotation,0.01) == snappedf(PI,0.01):
 		thrust.emit(ThrustDirection.global_position)
+	else:
+		ThrustParticles.emitting = false
