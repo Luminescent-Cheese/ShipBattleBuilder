09f822eb81d73e44cbcdcd22ac4870f2f7f3aecf
Asher Feinstein <asher.feinstein@gmail.com>
2026-06-26 11:16:56 -0400

    basic rotation
    nearly works, still a few problems with thrusters in either direction giving infinite spin if fast enough

--- ship.gd
+++ ship.gd
@@ -3,14 +3,57 @@ extends RigidBody2D
 @onready var Ship_Part = preload("res://ship_part.tscn")
 @onready var Thruster = preload("res://thruster.tscn")
 
+var force_dir = [Vector2.ZERO]
+var force_pos = [Vector2.ZERO]
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("test"):
 		var New_part = Ship_Part.instantiate()
 		add_child(New_part)
+		New_part.add_collision.connect(add_collision_shape)
 	if Input.is_action_just_pressed("test2"):
 		var New_thruster = Thruster.instantiate()
 		add_child(New_thruster)
 		New_thruster.thruster_on.connect( on_thrust)
+		New_thruster.add_collision.connect(add_collision_shape)
+		
+	$CenterOfMass.position = Vector2(0,0)
+	
+func add_collision_shape(set_position):
+	var collision_shape = CollisionShape2D.new()
+	collision_shape.position = set_position
+	collision_shape.shape = RectangleShape2D.new()
+	collision_shape.shape.size = Vector2(128,128)
+	add_child(collision_shape)
+	mass += 5
+	#recaulculates center of mass
+	calculate_center_of_mass()
+
 		
 func on_thrust(force_direction,force_position) -> void:
-	apply_force(force_direction,force_position) 
+	force_dir.append(force_direction.rotated(global_rotation))
+	force_pos.append(force_position)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	for i in force_dir.size():
+		apply_force(force_dir[i],force_pos[i])
+	if force_dir.size() > 1:
+		force_dir.clear()
+		force_pos.clear()
+
+func calculate_center_of_mass():
+	var positions = []
+	var averagePosition = Vector2.ZERO
+	#finds where new center of mass should be
+	for child in get_children():
+		if child is CollisionShape2D:
+			positions.append(child.position)
+	for i in positions.size():
+		averagePosition += positions[i]
+	averagePosition /= positions.size()
+	#makes it so that point becomes (0,0) locally
+	for child in get_children():
+		if not child is Camera2D:
+			child.position -= averagePosition
+	#changes global position so it looks like no movement occured
+	global_position += averagePosition
--- ship_part.gd
+++ ship_part.gd
@@ -5,7 +5,7 @@ var TILE_WIDTH
 @onready var tileSprite = $BaseShipSprite
 
 signal thruster_on
-
+signal add_collision
 func _ready() -> void:
 	TILE_WIDTH = tileSprite.get_rect().size.x
 
@@ -18,6 +18,7 @@ func place():
 		global_position = get_global_mouse_position().snapped(Vector2(TILE_WIDTH,TILE_WIDTH))
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			placeable = false
+			add_collision.emit(position)
 		if Input.is_action_just_pressed("rotate"):
 			rotation += PI/2
 			if snappedf(rotation,0.01) == snappedf(2*PI,0.01):
@@ -25,4 +26,7 @@ func place():
 
 
 func _on_thruster_forces_thrust(ThrustDirection) -> void:
-	thruster_on.emit(self.global_position.direction_to(ThrustDirection)*100,position)
+	thruster_on.emit(global_position+transform.y*-200,global_position-get_parent().global_position)
+	$"thrust center".global_position = global_position
+	$"thrust direction".global_position = (transform.y)*200
+	#print("thrust center = %s, dir = %s" % [$"thrust center".global_position, $"thrust direction".global_position])
