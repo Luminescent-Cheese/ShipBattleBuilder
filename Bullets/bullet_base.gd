b2672b42d13718a07702423173bff0e35080fa4c
Asher Feinstein <asher.feinstein@gmail.com>
2026-08-12 15:32:24 -0400

    gun particles

--- Bullets/bullet_base.gd
+++ Bullets/bullet_base.gd
@@ -4,10 +4,15 @@ extends CharacterBody2D
 @export var shoot_dir: Vector2 = Vector2.ZERO
 @export var damage: float = 5.0
 @export var parentVelocity: Vector2 = Vector2.ZERO
-
+func _ready() -> void:
+	$"Bullet Sprite".visible = false
 func _physics_process(delta: float) -> void:
 	velocity = (SPEED * shoot_dir)+ parentVelocity
 	move_and_slide()
 
 func _on_deletion_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_apperance_timer_timeout() -> void:
+	$"Bullet Sprite".visible = true
--- Tiles/gun_logic.gd
+++ Tiles/gun_logic.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var bullet = preload("res://Bullets/bullet_base.tscn")
 @onready var aim = $"../AimMark"
 @onready var ShotCooldown = $"../ShotCooldown"
+@onready var GunParticles = $"../GunFireParticle"
 
 @onready var canShoot = true
 @onready var lastPos = global_position
@@ -11,6 +12,7 @@ func _process(delta: float) -> void:
 	lastPos = currentPos
 	currentPos = global_position
 	if Input.is_action_pressed("Launch") and canShoot:
+		GunParticles.emitting = true
 		fire_bullet(delta)
 		canShoot = false
 		ShotCooldown.start()
